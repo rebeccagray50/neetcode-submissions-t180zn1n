5fa4690473b06b08ad3a2bd068dc29cd5cc843cc
Rebecca Gray <161752331+rebeccagray50@users.noreply.github.com>
2026-08-29 13:32:39 +0100

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = {} #maps index to value
+
+        #iterate through once to fill into map 
+        for i, val in enumerate(nums):
+            indices[val] = i #call value, get index
+        
+        #iterate to check 
+        for i, val in enumerate(nums):
+            diff = target - val
+            if diff in indices and i != indices[diff]:
+                return [i, indices[diff]]
