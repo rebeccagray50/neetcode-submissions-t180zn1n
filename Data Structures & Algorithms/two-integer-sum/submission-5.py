bd64692859e32f83e35b1fa808f6340c0479ea67
Rebecca Gray <161752331+rebeccagray50@users.noreply.github.com>
2026-08-30 13:19:54 +0100

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices  = {} #val -> index
+
+        #go through once and add to hm 
+        for i in range(0, len(nums)): 
+            indices[nums[i]] = i
+
+        #go through again checking for diff in hm 
+        for index, val in enumerate(indices): 
+            diff = target - val
+            if indices.get(diff) != index and diff in indices:
+                return [index, indices[diff]]
+        return [0 , 0]
