9ea8af115e0dc60c6637100538025026f625d0b4
Rebecca Gray <161752331+rebeccagray50@users.noreply.github.com>
2026-09-01 21:13:05 +0100

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        mem = []
+        for string in strs: 
+            #add length of string e.g. 4
+            mem.append(str(len(string)))
+            #add delimiter
+            mem.append("-")
+            #then string
+            mem.append(string)
+        return "".join(mem) #combines all elements end to end
+
+    def decode(self, s: str) -> List[str]:
+        mem = []
+        i = 0 
+
+        #until end of string is reached
+        while i < len(s): 
+            j = i 
+            #jump to next delimiter
+            while s[j] != '-':
+                j += 1 
+            
+            length = int(s[i:j])
+            i = j+1
+            j = i+length
+            mem.append(s[i:j])
+            i = j
+        
+        return mem 
