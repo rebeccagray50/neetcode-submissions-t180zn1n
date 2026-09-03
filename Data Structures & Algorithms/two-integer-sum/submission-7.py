7bc4f4dbfd7483e863c9ff5f1943d813ec8af30a
Rebecca Gray <161752331+rebeccagray50@users.noreply.github.com>
2026-09-03 10:05:15 +0100

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = {}
+
+        #add all to hm 
+        for i in range(0, len(nums)): 
+            indices[nums[i]] = i #num -> index
+
+        #iterate through hm, get diff, check if contains 
+        for index, num in enumerate(indices):
+            diff = target - num
+            if diff in indices and indices[diff] != index: 
+                return [index, indices[diff]]
